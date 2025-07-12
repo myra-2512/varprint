e4bfbbb206ed74a2e0b94a8ee7c5b06cb6e98644
myra-2512 <abhi.back2back@gmail.com>
2025-07-12 12:43:46 +0530

    Add files via upload

---
+++ mainhw.py
@@ -0,0 +1,11 @@
+a="May 2"
+b="June 15"
+c="July 20"
+d="August 5"
+e="September 10"
+
+print("Raj's birthday is on", a)
+print("Ravi's birthday is on", b)
+print("Rohan's birthday is on", c)
+print("Rita's birthday is on", d)
+print("Rani's birthday is on", e)
